0dd3b6134e8697402e4b6afde26625b10e9c381e
Shaundator <rash0081@stud.kea.dk>
2023-04-26 17:04:53 +0200

    commit

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,15 +1,35 @@
 import { Router } from 'express';
+import { rateLimit } from "express-rate-limit";
+
 import { getLoginPage } from '../util/pages.js';
 import { requireVisit } from './tools/requireVisit.js';
 import getUser from '../util/repository/getUser.js';
 
+
 const router = Router();
+
+const apiLimiter = rateLimit({
+  windowMs: 15,
+  max: 4,
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
+router.use(apiLimiter)
+
+router.use("/auth", rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 4,
+  standardHeaders: true,
+  legacyHeaders: false,
+}))
+
 
 router.get('/login', requireVisit, (req, res) => {
   res.send(getLoginPage())
 });
 
-router.post('/login', (req, res) => {
+router.post('/auth/login', (req, res) => {
   const user = getUser(req.body.emailInput, req.body.passwordInput);
   if(user === undefined){
     res.redirect('/login')
--- util/repository/getUser.js
+++ util/repository/getUser.js
@@ -1,14 +1,11 @@
 export default function getUser(email, password){
-    console.log('checking users');
     const user = users.find(user => user.name === email);
     if(user === undefined){
         return;
     }
     if(user.password === password){
-        console.log('same name and password');
         return user;
     } else {
-        console.log('same name and password');
         return;
     }
 }
